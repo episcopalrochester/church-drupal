feec7ee4373ad2b11a1aa4fa0c31f63921e1d3a9
Matt Townsend <matthew@episcopalrochester.org>
2013-07-16 14:32:02 +0000

    Added javascript to slideshow admin page

--- custom_modules/front_page_slideshow/front_page_slideshow.js
+++ custom_modules/front_page_slideshow/front_page_slideshow.js
@@ -3,4 +3,15 @@ var $ = jQuery;
 $(document).ready(function() {
   $('#front-slideshow').show();
   $('#front-slideshow .slide-text-container-inner').append('<div class="slide-controls"><a href="#" class="slide-prev">Prev</a><a href="#" class="slide-next">Next</a>');
+  var slide_count = $('#edit-slideshow-count').val();
+  if ( slide_count ) {
+    $('.admin-slide-table.active tbody tr:nth-child('+slide_count+')').addClass("last-slide");
+    $('#edit-slideshow-count').change(function() {
+      var slide_count = $(this).val();
+      $('.admin-slide-table.active tbody tr').each(function() {
+        $(this).removeClass('last-slide');
+      });
+      $('.admin-slide-table.active tbody tr:nth-child('+slide_count+')').addClass("last-slide");
+    });
+  }
 });
