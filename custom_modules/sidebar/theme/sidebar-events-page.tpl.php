a7d0c89962f02971ef77c9e42034a5e5d85eec61
Matt Townsend <matthew@episcopalrochester.org>
2013-07-19 16:49:19 +0000

    pager tweak, new template

---
+++ custom_modules/sidebar/theme/sidebar-events-page.tpl.php
@@ -0,0 +1,13 @@
+<div id="sidebar-upcoming-events">
+    <?php $date = false; foreach($events['nodes'] as $event): ?>
+    <?php if ($date != date("l, F j",$event->field_event_date['und'][0]['value'])): ?>
+      <?php $date = date("l, F j",$event->field_event_date['und'][0]['value']); ?>
+      <h3 class="title"><?php print $date; ?></h3>
+    <?php endif; ?>
+    <div class="sidebar-upcoming-event">
+      <?php print str_replace("<br /><br />","<br />",$event->body['und'][0]['value']); ?>
+    </div>
+  <?php endforeach; ?>
+</div>
+<?php print $events['pager']; ?>
+
