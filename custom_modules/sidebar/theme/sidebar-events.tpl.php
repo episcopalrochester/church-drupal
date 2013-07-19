dfcd8dd92eb82027d0bc685572601093d7ee05e9
Matt Townsend <matthew@episcopalrochester.org>
2013-07-19 15:33:57 +0000

    altered event prinout

--- custom_modules/sidebar/theme/sidebar-events.tpl.php
+++ custom_modules/sidebar/theme/sidebar-events.tpl.php
@@ -1,7 +1,15 @@
 <div id="sidebar-upcoming-events">
-  <?php foreach ($events as $event): ?>
+    <?php $date = false; foreach($events as $event): ?>
+    <?php if ($date != date("l, F j",$event->field_event_date['und'][0]['value'])): ?>
+      <?php $date = date("l, F j",$event->field_event_date['und'][0]['value']); ?>
+      <h3 class="title"><?php print $date; ?></h3>
+    <?php endif; ?>
     <div class="sidebar-upcoming-event">
       <?php print str_replace("<br /><br />","<br />",$event->body['und'][0]['value']); ?>
     </div>
   <?php endforeach; ?>
 </div>
+<div class="event-controls">
+<?php print l("See more events","upcoming-events"); ?>
+</div>
+
