6c0ca7053c7c65bae54363e06d91ab1855dd8348
Matt Townsend <matthew@episcopalrochester.org>
2013-08-12 20:46:08 +0000

    slideshow changes, front page adjustments

--- custom_modules/front_page/front_page.js
+++ custom_modules/front_page/front_page.js
@@ -1,5 +1,6 @@
 var $ = jQuery;
 $(document).ready(function() {
+ $("body.toolbar-drawer ul#toolbar-menu").prepend("<li><a href='/node/add'><strong>+</strong></a></li>");
  $("body.toolbar-drawer ul#toolbar-menu").append("<li><a href='/admin/structure/menu/manage/main-menu'>Menu</a></li>");
  $("body.toolbar-drawer ul#toolbar-menu").append("<li><a href='/admin/structure/block'>Blocks</a></li>");
  $("body.toolbar-drawer ul#toolbar-menu").append("<li><a href='/admin'>Admin</a></li>");
