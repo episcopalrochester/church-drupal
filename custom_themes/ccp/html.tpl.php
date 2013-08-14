813cb201ed070e9e90222db1926382d7b12bb693
Matt Townsend <matthew@episcopalrochester.org>
2013-08-14 18:32:43 +0000

    fixed head printing

--- custom_themes/ccp/html.tpl.php
+++ custom_themes/ccp/html.tpl.php
@@ -1,12 +1,13 @@
 <!DOCTYPE html>
 <html>
   <head>
+    <?php print $head; ?>
     <title><?php print $head_title; ?></title>
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
 <?php print $styles; ?>
 <?php print $scripts; ?>
   </head>
-  <body>
+  <body class="<?php print $classes; ?>" <?php print $attributes;?>>
 <?php print $page_top; ?>
 <?php print $page; ?>
 <?php print $page_bottom; ?>
