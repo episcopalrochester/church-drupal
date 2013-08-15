110af10d9fc1e24d59e6cd2303644cf1f02fd5f3
Matt Townsend <matthew@episcopalrochester.org>
2013-08-15 14:39:59 +0000

    adding more context links, external map link to sidebar

--- custom_modules/sidebar/theme/sidebar-map.tpl.php
+++ custom_modules/sidebar/theme/sidebar-map.tpl.php
@@ -1,2 +1,5 @@
 <p><div id="sidebar-church-map" data-map-address="<?php print $address; ?>">&nbsp;</div></p>
 <p><?php print str_replace("\n","<br />",$address); ?></p>
+<?php if ($external_url): ?>
+<p><?php print l("View larger map &raquo;",$external_url,array('html'=>TRUE,'attributes'=>array('target'=>'_blank'))); ?></p>
+<?php endif; ?>
