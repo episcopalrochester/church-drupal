665c4e6d3f680ddec12dfe737a4daab747ff5e10
Matt Townsend <matthew@episcopalrochester.org>
2013-08-26 19:44:25 +0000

    adding stewardship css

---
+++ custom_modules/stewardship/theme/stewardship.tpl.php
@@ -0,0 +1,16 @@
+<?php print $message['value']; ?>
+<h2>Stories of Giving</h2>
+<?php foreach ($stories['nodes'] as $story): ?>
+  <div class="stewardship-story">
+    <h3 class="stewardship-title"><h3><?php print l($story->title,"node/".$story->nid); ?></h3>
+    <div class="stewardship-image"><a href="<?php print url("node/".$story->nid); ?>"><?php
+      print theme_image_style(array('path' => $story->field_stewardship_featured_image['und'][0]['uri'], 'style_name' => 'stewardship_thumb','width'=>NULL,'height'=>NULL));
+    ?></a></div>
+    <p><?php print $story->body['und'][0]['summary']; ?></p>
+    <p><?php print l("Read more &raquo;","node/".$story->nid,array('html'=>true)); ?></p>
+  </div>
+<?php endforeach; ?>
+<h2>Ways of Giving</h2>
+<div class="ways-of-giving">
+<?php print $ways['value']; ?>
+</div>
