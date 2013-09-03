5b7cb19001f3c244ab27c05356a94ba7a9bd933b
Matt Townsend <matthew@episcopalrochester.org>
2013-09-03 18:41:30 +0000

    code cleanup and comments

--- custom_modules/sermons/theme/sermons.tpl.php
+++ custom_modules/sermons/theme/sermons.tpl.php
@@ -1,36 +1,35 @@
 <?php foreach ($sermons['nodes'] as $node): ?>
-<div class="sermon-teaser">
-<h2><?php print l($node->title,"node/".$node->nid); ?></h2>
-<?php if (isset($node->field_sermon_author['und'][0])): ?>
-<p><strong>By: </strong>
-<?php $author_nid = $node->field_sermon_author['und'][0]['nid'];
-$author = node_load($author_nid);
-print l($author->title,"node/".$author_nid); ?>
-</p>
-<?php endif; ?>
-<?php if (isset($node->field_sermon_date['und'][0])): ?>
-<p><em>
-<?php print date('l, F j, Y',$node->field_sermon_date['und'][0]['value']);
-?></em>
-</p>
-<?php endif; ?>
-
-<?php if (isset($node->field_sermon_video['und'][0])): ?>
-<p><?php print render(field_view_field('node',$node,'field_sermon_video',array(
-  'type' => 'file_rendered',
-  'label' => 'hidden',
-))); ?></p>
-<?php endif; ?>
-<?php if (isset($node->field_sermon_audio['und'][0])): ?>
-<p><?php print render(field_view_field('node',$node,'field_sermon_audio',array(
-  'type' => 'mediaelement_audio',
-  'label' => 'hidden',
-))); ?></p>
-<?php endif; ?>
-<p><?php print $node->body['und'][0]['summary']; ?></p>
-<p><?php print l("Read more &raquo;","node/".$node->nid,array('html'=>true)); ?></p>
-</div>
+  <div class="sermon-teaser">
+    <h2><?php print l($node->title,"node/".$node->nid); ?></h2>
+    <?php if (isset($node->field_sermon_author['und'][0])): ?>
+      <p><strong>By: </strong>
+        <?php $author_nid = $node->field_sermon_author['und'][0]['nid'];
+        $author = node_load($author_nid);
+        print l($author->title,"node/".$author_nid); ?>
+      </p>
+    <?php endif; ?>
+    <?php if (isset($node->field_sermon_date['und'][0])): ?>
+      <p><em>
+        <?php print date('l, F j, Y',$node->field_sermon_date['und'][0]['value']);
+        ?></em>
+      </p>
+    <?php endif; ?>
+    <?php if (isset($node->field_sermon_video['und'][0])): ?>
+      <p><?php print render(field_view_field('node',$node,'field_sermon_video',array(
+        'type' => 'file_rendered',
+        'label' => 'hidden',
+      ))); ?></p>
+    <?php endif; ?>
+    <?php if (isset($node->field_sermon_audio['und'][0])): ?>
+      <p><?php print render(field_view_field('node',$node,'field_sermon_audio',array(
+        'type' => 'mediaelement_audio',
+        'label' => 'hidden',
+      ))); ?></p>
+    <?php endif; ?>
+    <p><?php print $node->body['und'][0]['summary']; ?></p>
+    <p><?php print l("Read more &raquo;","node/".$node->nid,array('html'=>true)); ?></p>
+  </div>
 <?php endforeach; ?>
 <?php if ($sermons['pager']): ?>
-<?php  print $sermons['pager']; ?>
+  <?php  print $sermons['pager']; ?>
 <?php endif; ?>
