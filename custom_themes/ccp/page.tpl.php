de4da0793f29b1afb077ae49fef02078c3515f77
Matt Townsend <matthew@episcopalrochester.org>
2013-09-04 15:53:53 +0000

    theme code cleanup

--- custom_themes/ccp/page.tpl.php
+++ custom_themes/ccp/page.tpl.php
@@ -53,9 +53,9 @@
         <?php if ($tabs): ?><div id="tabs-wrapper" class="clearfix"><?php endif; ?>
         <?php print render($title_prefix); ?>
         <?php if ($title && !$is_front): ?>
-        <h1<?php print $tabs ? ' class="with-tabs"' : '' ?>><?php print $title ?></h1>
+          <h1<?php print $tabs ? ' class="with-tabs"' : '' ?>><?php print $title ?></h1>
         <?php elseif ($is_front): ?>
-        <h1 class="front-page-heading"><?php print variable_get("front_page_heading","Welcome"); ?></h1>
+          <h1 class="front-page-heading"><?php print variable_get("front_page_heading","Welcome"); ?></h1>
         <?php endif; ?>
         <?php print render($title_suffix); ?>
         <?php if ($tabs): ?><?php print render($tabs); ?></div><?php endif; ?>
@@ -63,13 +63,12 @@
         <?php print $messages; ?>
         <?php print render($page['help']); ?>
         <?php if ($action_links): ?><ul class="action-links"><?php print render($action_links); ?></ul><?php endif; ?>
-</div>
+      </div>
       <?php if ($is_front): ?>
         <?php if ($page['front_highlights']): ?>
           <?php print render($page['front_highlights']); ?>
         <?php endif; ?>
       <?php endif; ?>
-
       <div class="main-content-inner">
         <div class="clearfix">
           <?php print render($page['content']); ?>
