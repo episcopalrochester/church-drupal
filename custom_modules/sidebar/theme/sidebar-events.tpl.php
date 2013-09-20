ab024bc1e1b21e66c08350e56300327785f70784
Matt Townsend <matthew@episcopalrochester.org>
2013-09-20 18:35:02 +0000

    sticky events and spacing repair

--- custom_modules/sidebar/theme/sidebar-events.tpl.php
+++ custom_modules/sidebar/theme/sidebar-events.tpl.php
@@ -2,11 +2,14 @@
     <?php $date = false; foreach($events['nodes'] as $event): ?>
     <?php if ($date != date("l, F j",$event->field_event_date['und'][0]['value'])): ?>
       <?php $date = date("l, F j",$event->field_event_date['und'][0]['value']); ?>
-      <h3 class="title"><?php print $date; ?></h3>
+      <h3 class="title"><?php if ($event->sticky == 1):?>Featured: <?php endif; ?><?php print $date; ?></h3>
     <?php endif; ?>
     <div class="sidebar-upcoming-event">
-      <?php print str_replace("<br /><br />","<br />",$event->body['und'][0]['value']); ?>
+      <?php print strip_tags($event->body['und'][0]['value'],"<strong><b>"); ?>
     </div>
+    <?php if (node_access("update",$event)): ?>
+      <p><?php print l("edit","node/".$event->nid."/edit"); ?></p>
+    <?php endif; ?>
   <?php endforeach; ?>
 </div>
 <div class="event-controls">
